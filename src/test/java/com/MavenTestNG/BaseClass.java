c3e0ee173a18d9cc33d2bdb9a293a250fb2754b2
khanmoham85 <khanmoham85@gmail.com>
2018-12-29 12:24:56 -0500

    push will fail

--- src/test/java/com/MavenTestNG/BaseClass.java
+++ src/test/java/com/MavenTestNG/BaseClass.java
@@ -1,11 +1,15 @@
 package com.MavenTestNG;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class BaseClass {
 
+	WebDriver driver;
 	@Test
 	public void setup() {
 		System.setProperty("webdriver.chrome.driver", "Drivers/chromedriver.exe");
@@ -13,5 +17,11 @@ public class BaseClass {
 		driver.get("https://www.google.com/");
 		driver.quit();
 		
+	}
+	@Test
+	public void startup() {
+		boolean searchbar = driver.findElement(By.xpath("//input[@class='gLFyf gsfi']")).isDisplayed();
+		
+		Assert.assertEquals(12,13);
 	}
 }
